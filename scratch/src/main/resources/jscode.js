d840caedbf7c8ea3460a5d81e7c51b1ea2f5711e
Raphael Moita <raphael.moita@gmail.com>
2018-03-23 14:30:15 +0100

    Nashorn sample

---
+++ scratch/src/main/resources/jscode.js
@@ -0,0 +1,27 @@
+var greeting = 'hey, helloooooo world';
+
+// Java class being instantiated 
+var javaClass = Java.type('com.xxx.yyy.scratch_3');
+
+// 'name' and 'surname' are being bind in Java side
+var fullName = name  + " " + surname
+
+// loading external library
+load('file:///C://Users//lucasmor//temp//underscore-min.js');
+// 'numbers' is being bind in Java side
+var odds = _.filter(numbers, function (num) {
+    print("num: " + num)
+    return num % 2 == 1;
+});
+print("Odds from " + numbers + " are: " + odds); 
+
+// class scratch_3 that contains a public member called ID
+print("Java Object: " + javaObj.ID);
+
+function getMsg() {
+	print('calling getMsg ...');
+	// calling Java class
+	return javaClass.process("Hello " + fullName);
+}
+// return statement with
+getMsg();
